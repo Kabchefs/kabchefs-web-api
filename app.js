6093a2542ba2bc65b992170efbe60d893cdcd3cf
Himanshi Maurya <mauryahimanshi9898@gmail.com>
2020-07-28 22:50:02 +0530

    login submit

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 
 const mongoose = require("mongoose");
-const express = require("express");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
-const dbString = process.env.DATABASE ;
+dotenv.config();
+//Connect to Mongo
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-//DB Connection
-mongoose
-  .connect(dbString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("DB CONNECTED");
-  });
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
-//Middlewares
-app.use(bodyParser.json());
-app.use(cors());
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// const dbString = process.env.DATABASE
+// //DB Connection
+// mongoose
+//   .connect(dbString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log("DB CONNECTED");
+//   });
+
+// //Middlewares
+// app.use(bodyParser.json());
+// app.use(cors());
 
 
-//PORT
-const port = process.env.PORT || 8000;
+// //PORT
+// const port = process.env.PORT || 8000;
 
 //Starting a server
-app.listen(port, () => {
-  console.log(`app is running at ${port}`);
-});
+app.listen(PORT, console.log(`app is running at ${PORT}`));
